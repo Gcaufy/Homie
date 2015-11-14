372171502a92b00db2ea8bda47e8f6e6354d8e8a
Aaron <luanjiwu5@gmail.com>
2015-11-14 12:05:27 +0800

    init commit

---
+++ client/Homie/Homie/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  Homie
+//
+//  Created by Aaron on 11/14/15.
+//  Copyright © 2015 Aaron. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+
+@end
