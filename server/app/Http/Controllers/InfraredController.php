13292c793d86bd65089ac86370e4db3960d0a1f2
Roy Xiang <git@royxiang.me>
2015-11-15 01:56:50 +0800

    get the latest record correctly for the very first request

--- server/app/Http/Controllers/InfraredController.php
+++ server/app/Http/Controllers/InfraredController.php
@@ -14,16 +14,15 @@ class InfraredController extends Controller
 
     public function getLatest($time)
     {
-        $data = ['changed' => '0', 'time' => '0'];
-        if (!empty($time)) {
-            $model = Infrared::where('time', '>', $time)->orderBy('time', 'desc')->first();
-            if ($model !== null) {
-                $data['changed'] = '1';
-                $data['time'] = $model->time;
-            } else {
-                $data['time'] = $time;
-            }
+        $time = empty($time) ? '0' : $time;
+        $data = ['changed' => '0', 'time' => $time];
+
+        $model = Infrared::where('time', '>', $time)->orderBy('time', 'desc')->first();
+        if ($model !== null) {
+            $data['changed'] = '1';
+            $data['time'] = $model->time;
         }
+
         return response()->json($data);
     }
 }
